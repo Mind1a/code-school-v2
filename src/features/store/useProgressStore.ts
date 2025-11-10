957bb83cddd835867a01662a468c95c50a2b1a50
Archil Gelashvili <ar.gelashvili@gmail.com>
2025-11-10 12:05:15 +0400

    feature/updated context stores

--- src/features/store/useProgressStore.ts
+++ src/features/store/useProgressStore.ts
@@ -8,7 +8,7 @@ type ProgressState = {
   setProgress: (value: number) => void;
 };
 
-export const useProgressStore = create<ProgressState>()(
+export const useHtmlProgressStore = create<ProgressState>()(
   persist(
     (set) => ({
       progress: 0,
@@ -21,7 +21,25 @@ export const useProgressStore = create<ProgressState>()(
       resetProgress: () => set({ progress: 0 }),
     }),
     {
-      name: "lesson-progress",
+      name: "html-lesson-progress",
+    }
+  )
+);
+
+export const usePythonProgressStore = create<ProgressState>()(
+  persist(
+    (set) => ({
+      progress: 0,
+      increaseProgress: (amount) =>
+        set((state) => ({
+          progress: Math.min(100, state.progress + amount),
+        })),
+      setProgress: (value) =>
+        set({ progress: Math.min(100, Math.max(0, value)) }),
+      resetProgress: () => set({ progress: 0 }),
+    }),
+    {
+      name: "python-lesson-progress",
     }
   )
 );
