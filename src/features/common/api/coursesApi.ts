e79c9b6f09b1780e209034e2c4789e42620c8908
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-12-19 03:23:24 +0400

    feature/separate-htmlBasic

--- src/features/common/api/coursesApi.ts
+++ src/features/common/api/coursesApi.ts
@@ -16,3 +16,12 @@ export const CourseByIdApi = async (id: string) => {
   if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
   return res.json();
 };
+
+export const HomeworkByIdApi = async (homeworkId: string) => {
+  const res = await fetch(`${baseUrl}/api/homeworks/${homeworkId}`, {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+  return res.json();
+};
