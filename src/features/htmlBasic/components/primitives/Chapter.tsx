03d0464e2a97d71b76f6738aee90ba2808bd80c6
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-12-19 19:59:20 +0400

    feature/dynamic-chapter-page

--- src/features/htmlBasic/components/primitives/Chapter.tsx
+++ src/features/htmlBasic/components/primitives/Chapter.tsx
@@ -1,10 +1,7 @@
 'use client';
 import Image from 'next/image';
 import { useQuery } from '@tanstack/react-query';
-import {
-  ChapterByIdApi,
-  HomeworkByIdApi,
-} from '@/features/common/api/coursesApi';
+import { ChapterByIdApi } from '@/features/common/api/coursesApi';
 import HomeworkSkeleton from './HomeworkSkeleton';
 import { HomeworkProps } from '../../type';
 
@@ -14,60 +11,68 @@ const Chapter = ({
   chapterId,
 }: HomeworkProps) => {
   if (!chapterId) return <p>No chapter selected.</p>;
-  const {
-    data: homework,
-    isLoading,
-    isError,
-  } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['chapter', chapterId],
     queryFn: () => ChapterByIdApi(chapterId),
   });
   if (isLoading) return <HomeworkSkeleton />;
-  if (isError || !homework) return <p>Error loading homework.</p>;
+  if (isError || !data) return <p>Error loading homework.</p>;
 
   return (
-    <div className="flex-1 bg-[#f8feff] px-[24px] py-[36px] border border-[#b7dae0] rounded-xl">
-      <div className="flex justify-between items-center mb-[32px] min-h-[50px]">
-        <p className="bg-[#454545] px-[24px] py-1 rounded-[8px] font-bold text-white text-2xl">
-          დავალება #1
-        </p>
-        <button onClick={() => setIsSidebarVisible((prev) => !prev)}>
-          <Image
-            src={
-              isSidebarVisible
-                ? '/images/svg/ScaleUp.svg'
-                : '/images/svg/ScaleDown.svg'
-            }
-            alt="arrows"
-            width={22}
-            height={22}
-            className="w-[22px] h-[22px] cursor-pointer"
-          />
-        </button>
-      </div>
-
-      <div className="text-[#454545]">
-        <div className="mb-[15px] text-[18px] leading-[32px]">
-          <span className="font-semibold text-lg">დავალების პირობა:</span>
-          {homework.question}
-        </div>
-
-        <div className="mb-5">
-          <div className="flex items-center gap-[13px] mb-[8px] min-h-[36px]">
-            <p className="font-bold text-[#454545]">დახმარება</p>
+    <div className="flex flex-col flex-1 justify-between bg-[#f8feff] px-[20px] py-[20px] border border-[#b7dae0] rounded-xl">
+      <div>
+        <div className="flex justify-between items-center mb-[8px] min-h-[50px]">
+          <p className="font-bold text-[#454545] text-[24px]">
+            თავი #{data.chapter.chapterNumber}
+          </p>
+          <button onClick={() => setIsSidebarVisible((prev) => !prev)}>
             <Image
-              src="/images/svg/help.svg"
-              alt="help"
-              width={15}
-              height={15}
+              src={
+                isSidebarVisible
+                  ? '/images/svg/ScaleUp.svg'
+                  : '/images/svg/ScaleDown.svg'
+              }
+              alt="arrows"
+              width={22}
+              height={22}
+              className="w-[22px] h-[22px] cursor-pointer"
             />
+          </button>
+        </div>
+        <div className="bg-[#374669] w-full h-[1px]"></div>
+        <div>
+          <div className="mt-[12px]">
+            <p className="font-bold text-[#454545] text-[22px] leading-[32px]">
+              {data.chapter.chapterNumber} {data.chapter.chapterTitle}
+            </p>
           </div>
-
-          <div className="bg-[#f0f0f0] px-[16px] py-[12px] rounded-[12px]">
-            <p className="text-[18px] leading-[32px]">1</p>
+          <div className="flex flex-col gap-[16px] mt-[16px]">
+            <p className="flex gap-[10px] text-[#454545] leading-[32px]">
+              <span className="font-bold text-[18px]">
+                საკითხის განმარტება:
+              </span>
+              <span className="font-light">{data.chapter.description}</span>
+            </p>
+            <p className="flex gap-[10px] text-[#454545] leading-[32px]">
+              <span className="font-bold text-[18px]">
+                რეალური ცხოვრების მაგალითი:
+              </span>
+              <span className="font-light">{data.chapter.realLifeExample}</span>
+            </p>
           </div>
         </div>
       </div>
+
+      {data.chapter.imageUrl ? (
+        <Image
+          src={data.chapter.imageUrl}
+          alt="examplePhotoForStudy"
+          width={302}
+          height={242}
+        />
+      ) : (
+        ''
+      )}
     </div>
   );
 };
